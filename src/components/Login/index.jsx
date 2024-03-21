dccdc777153c0d0f077f2ae5b38975b28ae41b9a
Jesus De La Cruz <jdaviddelacruz@unicesar.edu.co>
2024-03-20 21:08:49 -0500

    Creacion componente Login

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,34 @@
+import './styles.css'
+
+const Login = () => {
+	
+
+	return (
+		<div className="login-form">
+        <div className="containe">
+            <div className="main">
+                <div className="content">
+                    <h2>BibilioWeb</h2>
+                    <form action="#" method="post">
+                        <input type="text" name="" placeholder="Correo Electronico" required autofocus=""/>
+                        <input type="password" name="" placeholder="Contraseña" required autofocus=""/>
+                         <button className="btn" type="submit">
+                            Iniciar Sesion
+                         </button>
+
+                    </form>
+                    <p className="account">¿No tienes cuenta? <a href="#">Registrar</a></p>
+                     
+                </div>
+                <div className="form-img">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+	);
+};
+
+export default Login;
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,16 @@
+
+const Main = () => {
+	
+	return (
+		<div >
+			<nav >
+				<h1>fakebook</h1>
+				<button >
+				  Cerrar sesión
+				</button>
+			</nav>
+		</div>
+	);
+};
+
+export default Main;
