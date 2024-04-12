bca3e6c3639edebf0b2628c0766e43cb57919097
jhonnierdlc <jhonierg20@gmail.com>
2024-04-12 07:24:13 -0500

    eliminar libro listo

--- src/components/Libros/ConsultaLibros/ConsultaLibro.jsx
+++ src/components/Libros/ConsultaLibros/ConsultaLibro.jsx
@@ -1,11 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./ConsultaLibro.css";
 import { Link } from "react-router-dom";
-import { obtenerLibro } from "../../../services/libroServices";
+import { eliminarLibro, obtenerLibro } from "../../../services/libroServices";
+import ModalEliminar from "../../UI/ModalEliminar";
+import { toast } from "react-toastify";
 
 const ConsultarLibro = () => {
-  // Datos de prueba para los libros
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [idAEliminar, setIdAEliminar] = useState(null);
+
   const [libros, setLibros] = useState([]);
+
+  const handleEliminarClick = (id) => {
+    setIdAEliminar(id);
+    setMostrarModal(true);
+  };
+
+  const handleAceptarEliminar = async (id) => {
+    const originalData = [...libros];
+    try {
+      const updatedLibros = originalData.filter((libro) => libro._id !== id);
+      setLibros(updatedLibros);
+      const response = await eliminarLibro(id);
+      toast.success(response.data);
+    } catch (error) {
+      toast.error(error.response?.data || "Error al eliminar Libro");
+      setLibros(originalData);
+    } finally {
+      setMostrarModal(false);
+    }
+    setMostrarModal(false);
+  };
 
   useEffect(() => {
     // Realiza la solicitud para obtener la lista de libros cuando el componente se monte
@@ -33,6 +58,26 @@ const ConsultarLibro = () => {
       <div className="libros-container">
         {libros.map((libro, index) => (
           <div className="libro" key={index}>
+            <div className="container-icons">
+              <Link
+                className="img-delete"
+                onClick={() => handleEliminarClick(libro._id)}
+              >
+                <img
+                  src="https://cdn.icon-icons.com/icons2/1808/PNG/512/trash-can_115312.png"
+                  alt="Eliminar"
+                  height={20}
+                />
+              </Link>
+              <Link className="img-pencil">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/51/51648.png"
+                  alt="Pencil"
+                  height={20}
+                />
+              </Link>
+            </div>
+
             <img
               src={libro.portada}
               alt="Portada del libro"
@@ -65,6 +110,13 @@ const ConsultarLibro = () => {
           </div>
         ))}
       </div>
+      {mostrarModal && (
+        <ModalEliminar
+          eliminarId={handleAceptarEliminar}
+          setMostrarModal={setMostrarModal}
+          idAEliminar={idAEliminar}
+        />
+      )}
     </>
   );
 };
--- src/components/UI/ModalEliminar.jsx
+++ src/components/UI/ModalEliminar.jsx
@@ -9,7 +9,12 @@ function ModalEliminar({ eliminarId, setMostrarModal, idAEliminar }) {
           <p>¿Estás seguro de que deseas eliminar?</p>
           <div>
             <button onClick={() => eliminarId(idAEliminar)}>Aceptar</button>
-            <button onClick={() => setMostrarModal(false)}  style={{ marginLeft: '8px', background:"#009879" }} >Cancelar</button>
+            <button
+              onClick={() => setMostrarModal(false)}
+              style={{ marginLeft: "8px", background: "#009879" }}
+            >
+              Cancelar
+            </button>
           </div>
         </div>
       </div>
